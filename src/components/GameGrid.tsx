914b72e6db3c1afbf03e7355e4fc81e06cb934ca
Ankit Chauhan <anchauha@adobe.com>
2023-06-03 13:30:23 +0530

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ function GameGrid() {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={10}
-        padding={"10px"}
+        padding={"3px"}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
