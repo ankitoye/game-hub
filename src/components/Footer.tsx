c378b0ae5c574419e8b00614a0e3d5195c16e85c
Ankit Chauhan <anchauha@adobe.com>
2023-06-07 11:24:04 +0530

    Added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { HStack, Text } from "@chakra-ui/react";
+
+function Footer() {
+  return (
+    <HStack padding={"10px"} textAlign={"center"}>
+      <Text textAlign={"center"} fontSize={"2xs"}>
+        Developed by Ankit Chauhan
+      </Text>
+    </HStack>
+  );
+}
+
+export default Footer;
